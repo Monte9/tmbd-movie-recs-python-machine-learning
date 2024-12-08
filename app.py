f5d02ef06d360d7bfae87ed85de9137079b06552
Monte Thakkar <manthan.thakkar@gmail.com>
2024-12-08 10:42:53 -0800

    use streamlit in app.py

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print("hello world")
+import streamlit as st
+
+st.title('Hello World!')
+
+st.write('## Hello World')
+
+st.metric('Metric', 42)
+
+st.text_input('Text Input')
+
+st.number_input('Number Input')
